d22966d8b7ca28e32668c1eb75f8f73a49e33342
Manraj <manrajkhokhar1995@gmail.com>
2020-06-18 00:05:22 -0700

    add palindrome linked list

--- easy/Linked List/palindrome_linked_list.js
+++ easy/Linked List/palindrome_linked_list.js
@@ -10,12 +10,38 @@
  * @return {boolean}
  */
 var isPalindrome = function (head) {
-    let array = [];
-    let p1 = head;
-    while (p1 != null) {
-        array.push(p1.val);
-        p1 = p1.next;
+    // let array = [];
+    // let p1 = head;
+    // while( p1 != null){
+    //     array.push(p1.val);
+    //     p1 = p1.next;
+    // }
+    // return JSON.stringify(array) === JSON.stringify(array.reverse());
+    let fast = head;
+    let slow = head;
+    while (fast !== null && fast.next !== null) {
+        fast = fast.next.next;
+        slow = slow.next
     }
-    return JSON.stringify(array) === JSON.stringify(array.reverse());
+
+    fast = head;
+    slow = reverse(slow);
+    while (slow !== null) {
+        if (slow.val !== fast.val) return false;
+        slow = slow.next;
+        fast = fast.next;
+    }
+    return true;
 
 };
+
+let reverse = function (head) {
+    let prevNode = null;
+    while (head !== null) {
+        let nextNode = head.next;
+        head.next = prevNode;
+        prevNode = head;
+        head = nextNode;
+    }
+    return prevNode;
+}
